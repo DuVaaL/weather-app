1a0f0c9be70373a685bcd36925a05a7ffcbae564
Duval <ngounoukenhem@gmail.com>
2020-04-26 13:40:14 +0100

    Updates

---
+++ precache-manifest.59148c665f88b7a3caa8e744a64e15d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8a9919a21763a66613c24b3dd3c850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520501831594ebf53168",
+    "url": "/static/css/2.580795d1.chunk.css"
+  },
+  {
+    "revision": "b91bf5a8a649f266d0bd",
+    "url": "/static/css/main.d7a21258.chunk.css"
+  },
+  {
+    "revision": "520501831594ebf53168",
+    "url": "/static/js/2.c5f729f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5f729f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91bf5a8a649f266d0bd",
+    "url": "/static/js/main.0bfc284e.chunk.js"
+  },
+  {
+    "revision": "0800dffc2738f0d5cf59",
+    "url": "/static/js/runtime-main.e09fc0f4.js"
+  },
+  {
+    "revision": "40127a5e21b8353d8d01232ef17bd221",
+    "url": "/static/media/weather.40127a5e.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
